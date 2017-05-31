66b4433bf91aacb4cd4548c782cb1ded6b466f4f
Sergey <sabarovsky@yahoo.com>
2017-05-31 20:00:44 +0300

    formatting updated

--- scrapy_quotes/spiders/quotes.py
+++ scrapy_quotes/spiders/quotes.py
@@ -10,7 +10,10 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for block_quote in response.xpath("//*[@class='quote']"):
             yield {
-                "text": block_quote.xpath(".//*[@class='text']/text()").extract_first(),
-                "author": block_quote.xpath(".//*[@class='author']/text()").extract_first(),
-                "tags": block_quote.xpath(".//*[@class='tag']/text()").extract(),
+                "text": block_quote.xpath(
+                    ".//*[@class='text']/text()").extract_first(),
+                "author": block_quote.xpath(
+                    ".//*[@class='author']/text()").extract_first(),
+                "tags": block_quote.xpath(
+                    ".//*[@class='tag']/text()").extract(),
             }
