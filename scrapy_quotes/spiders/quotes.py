1dcf2a977ba455ad2b1a1b804be21c01cc2e8c81
Sergey <sabarovsky@yahoo.com>
2017-06-04 18:56:09 +0300

    check

--- scrapy_quotes/spiders/quotes.py
+++ scrapy_quotes/spiders/quotes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
 from scrapy.http import Request
+from scrapy_quotes.items import ScrapyQuotesItem
 
 
 class QuotesSpider(Spider):
@@ -10,17 +11,33 @@ class QuotesSpider(Spider):
 
     def parse(self, response):
         for block_quote in response.xpath("//*[@class='quote']"):
-            yield {
-                "tags": block_quote.xpath(
-                    ".//*[@class='tag']/text()").extract(),
-                "text": block_quote.xpath(
-                    ".//*[@class='text']/text()").extract_first(),
-                "author": block_quote.xpath(
-                    ".//*[@class='author']/text()").extract_first(),
-                "born": "pass",
-                "description": "pass",
-            }
+            item = ScrapyQuotesItem()
+            request = Request(response.urljoin(block_quote.xpath(
+                ".//span/a/@href").extract_first()),
+                callback=self.parse_author_page)
+            item["tags"] = block_quote.xpath(
+                ".//*[@class='tag']/text()").extract()
+            item["text"] = block_quote.xpath(
+                ".//*[@class='text']/text()").extract_first()
+            item["author"] = block_quote.xpath(
+                ".//*[@class='author']/text()").extract_first()
+            yield item, request
+            # yield {
+            #     "tags": block_quote.xpath(
+            #         ".//*[@class='tag']/text()").extract(),
+            #     "text": block_quote.xpath(
+            #         ".//*[@class='text']/text()").extract_first(),
+            #     "author": block_quote.xpath(
+            #         ".//*[@class='author']/text()").extract_first(),
+            #     "born": data,
+            #     "description": "pass",
+            # }
         next_page = response.urljoin(response.xpath(
             "//*[@class='next']/a/@href").extract_first())
         yield Request(next_page)
-# response.urljoin(block_quote.xpath(".//span/a/@href").extract_first())
+
+    def parse_author_page(self, response):
+        item = ScrapyQuotesItem()
+        item["born"] = ' '.join(response.xpath(
+            "//*[@class='author-born-date' or @class='author-born-location']//text()").extract())
+        yield item
